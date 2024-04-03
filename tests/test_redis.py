b0ff6291966d87f4f43009257308495b00a21364
tonshlee <tonshlee@gmail.com>
2024-04-03 04:26:11 +0000

    testcontainer for redis tests

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,6 +1,7 @@
 import os
-from unittest import mock
+from unittest import TestCase
 from redis import Redis
+from testcontainers.redis import RedisContainer
 
 
 def redis_client() -> Redis:
@@ -15,7 +16,33 @@ def get_users() -> list[str]:
     return redis_client().lrange("test.user.names", 0, -1)  # type: ignore
 
 
-@mock.patch("tests.test_redis.redis_client")
-def test_get_users(mock_redis_client):
-    mock_redis_client.return_value.lrange.return_value = ["Alice", "Bob", "Charlie"]
-    assert get_users() == ["Alice", "Bob", "Charlie"]
+class TestRedisClient(TestCase):
+
+    def setUp(self) -> None:
+        self.container = RedisContainer(image="redis:latest")
+        self.container.start()
+
+        # mock redis server to redis testcontainer
+        os.environ["REDIS_SERVER_HOST"] = self.container.get_container_host_ip()
+        os.environ["REDIS_SERVER_PORT"] = self.container.get_exposed_port(6379)
+
+        self.redis_client = redis_client()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.container.stop()
+        return super().tearDown()
+
+    def init_users(self):
+        """ Initialize users in redis """
+        users = ["Alice", "Bob", "Charlie"]
+        for user in users:
+            self.redis_client.lpush("test.user.names", user)
+
+        return users
+
+    def test_get_users(self):
+        users = self.init_users()
+
+        for user in get_users():
+            assert user in users
